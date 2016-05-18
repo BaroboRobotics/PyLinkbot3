fe5d059f75f7029225953f327fe7d7d838feda6d
David Ko <david@barobo.com>
2016-05-18 11:11:11 -0700

    Bumped to version 3.0.0a2

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ README = codecs.open(os.path.join(here, 'README.txt'), encoding='utf8').read()
 setup (name = 'PyLinkbot3',
        author = 'David Ko',
        author_email = 'david@barobo.com',
-       version = '3.0.0a1',
+       version = '3.0.0a2',
        description = "This is a pure Python implementation of PyLinkbot. See http://github.com/BaroboRobotics/PyLinkbot",
        long_description = README,
        package_dir = {'':'src'},
