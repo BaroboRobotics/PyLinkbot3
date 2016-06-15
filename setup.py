7962d1ee3cbc0b51b222095c592a7386ffac4e8c
David Ko <david@barobo.com>
2016-06-15 14:41:14 -0700

    Bumped to version 3.0.5

--- setup.py
+++ setup.py
@@ -13,14 +13,14 @@ README = codecs.open(os.path.join(here, 'README.txt'), encoding='utf8').read()
 setup (name = 'PyLinkbot3',
        author = 'David Ko',
        author_email = 'david@barobo.com',
-       version = '3.0.4',
+       version = '3.0.5',
        description = "This is a pure Python implementation of PyLinkbot. See http://github.com/BaroboRobotics/PyLinkbot",
        long_description = README,
        package_dir = {'':'src'},
        packages = ['linkbot3', 'linkbot3.async'],
        url = 'http://github.com/BaroboRobotics/PyLinkbot3',
        install_requires=[
-           'PyRibbonBridge>=0.0.6', 
+           'PyRibbonBridge>=0.0.8', 
            'PySfp>=0.1.1', 
            'websockets>=3.0',],
        classifiers=[
