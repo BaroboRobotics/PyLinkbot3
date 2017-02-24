23898c88409a92115f73643f2418f4d88a4250f6
David Ko <david@barobo.com>
2017-02-24 13:12:10 -0800

    Added CLinkbot to publicly exposed classes

--- src/linkbot3/__init__.py
+++ src/linkbot3/__init__.py
@@ -11,7 +11,7 @@ import math
 import os
 import websockets
 
-__all__ = ['FormFactor', 'Linkbot', 'Daemon', ]
+__all__ = ['FormFactor', 'Linkbot', 'Daemon', 'CLinkbot']
 __all__ += async.__all__
 
 class Daemon():
