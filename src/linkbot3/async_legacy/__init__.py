02c94d3deef7070676a634820811f9e7cd194132
David Ko <david@barobo.com>
2018-05-10 12:26:32 -0700

    Only import sfp if we need it

--- src/linkbot3/async_legacy/__init__.py
+++ src/linkbot3/async_legacy/__init__.py
@@ -2,7 +2,6 @@ import asyncio
 import logging
 import os
 import ribbonbridge as rb
-import sfp
 import sys
 import websockets
 
@@ -90,6 +89,7 @@ class _AsyncLinkbot(rb.Proxy):
                     'ws://'+my_config.daemon_host[0]+':'+my_config.daemon_host[1], loop=self._loop)
         else:
             self.__log('Creating tcp connection to daemon...')
+            import sfp
             (transport, protocol) = yield from sfp.client.connect(
                     my_config.daemon_host[0], my_config.daemon_host[1], loop=self._loop)
 
